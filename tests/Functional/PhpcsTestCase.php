7edfecad2959960a328cb805e1c8ef077a0d2b6c
thomasooo <tomas.ballon@gmail.com>
2025-12-09 07:43:00 +0100

    chore(phpcs): adds more whitespace rules to coding standards (#22)
    https://pixelfederation.atlassian.net/browse/TS2-18278

---
+++ tests/Functional/PhpcsTestCase.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PixelFederation\CodingStandards\Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class PhpcsTestCase extends TestCase
+{
+    public static function assertPhpcbf(
+        string $fileBefore,
+        string $fileAfter,
+        string $message = '',
+        string $tmpFilename = 'Bar.php',
+    ): void {
+        $before = __DIR__ . '/' . $fileBefore;
+        $expected = __DIR__ . '/' . $fileAfter;
+
+        self::assertFileExists($before, 'Before file does not exist: ' . $before);
+        self::assertFileExists($expected, 'After file does not exist: ' . $expected);
+
+        $tmpDir = sys_get_temp_dir() . '/cs_bar_' . bin2hex(random_bytes(4));
+        self::assertTrue(mkdir($tmpDir, 0777, true), $message);
+
+        $tmpFile = $tmpDir . '/' . $tmpFilename;
+        self::assertNotFalse($tmpFile, $message);
+        file_put_contents($tmpFile, file_get_contents($before));
+
+        $cmdCbf = sprintf(
+            '%s --standard=%s %s 2>&1',
+            escapeshellarg(self::getPath('PATH_PHPCBF')),
+            escapeshellarg(self::getPath('PATH_PHPCS_RULESET')),
+            escapeshellarg($tmpFile),
+        );
+
+        $beforeContent = file_get_contents($before);
+        file_put_contents($tmpFile, $beforeContent);
+
+        exec($cmdCbf, $outputCbf, $exitCodeCbf);
+        self::assertSame(0, $exitCodeCbf, $message);
+
+        $actual = file_get_contents($tmpFile);
+        self::assertNotSame(
+            $beforeContent,
+            $actual,
+            'Expected PHPCBF to change the file, but contents are identical.' . $message,
+        );
+
+        self::assertSame(
+            file_get_contents($expected),
+            $actual,
+            "PHPCBF output does not match expected 'after' file. " . $message,
+        );
+
+        $cmdCs = sprintf(
+            '%s --standard=%s %s 2>&1',
+            escapeshellarg(self::getPath('PATH_PHPCS')),
+            escapeshellarg(self::getPath('PATH_PHPCS_RULESET')),
+            escapeshellarg($tmpFile),
+        );
+
+        exec($cmdCs, $outputCs, $exitCodeCs);
+
+        self::assertSame(
+            0,
+            $exitCodeCs,
+            sprintf(
+                "Expected no PHPCS errors after PHPCBF fix, got exit code %s.\nOutput:\n%s %s",
+                $exitCodeCs,
+                implode("\n", $outputCs),
+                $message,
+            ),
+        );
+
+        @unlink($tmpFile);
+    }
+
+    protected static function getPath(string $env): string
+    {
+        return __DIR__ . '/../../' . getenv($env);
+    }
+}
---
+++ tests/Functional/SuperfluousWhitespace/Bar.SuperfluousWhitespace.after.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PixelFederation\CodingStandards\Tests\Functional\SuperfluousWhitespace;
+
+final class Bar
+{
+    public readonly int $superNumber;
+
+    /**
+     * Superfluous docblock parameters
+     */
+    public function __construct(
+        public readonly int $width,
+        public readonly int $height,
+    ) {
+        $total = $this->width + $this->height;
+
+        $heightDoubled = $this->height * 2;
+        $this->superNumber = $total + $heightDoubled;
+    }
+}
